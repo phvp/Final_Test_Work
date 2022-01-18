78b5a6632ad8ac9a5a4943cf2e09b0675baf05f2
Philipp <phvp888@gmail.com>
2022-01-19 00:57:15 +0700

    Program.cs comments added

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+//Прямой вариант решения:
 int [] numbers = new int[] {1,2,3,4,5,6,7,8,9};
 Console.WriteLine("[{0}]->", string.Join(" ", numbers));
 Console.WriteLine("->");
@@ -6,3 +7,4 @@ for (int i = 0; i < numbers.Length; i++)
     if (numbers[i] % 2 == 0)
     Console.WriteLine("[{0}]", string.Join(" ", numbers[i]));
 }
+//Решение с применением методов:
